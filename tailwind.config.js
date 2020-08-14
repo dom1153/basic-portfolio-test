0fcc27d8c78c4701a6ca333331e0147f589cc4a6
Dominic Dang <dtdd1153@live.com>
2020-08-14 00:57:27 -0500

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// tailwind.config.js
+module.exports = {
+  purge: [
+    './*.html',
+  ],
+  theme: {
+    zIndex: {
+      '0': 0,
+      '10': 10,
+      '20': 20,
+      '30': 30,
+      '40': 40,
+      '50': 50,
+      '-1': '-1',
+      '-2': '-2',
+    },
+    visibility: ['responsive', 'hover', 'focus'],
+  },
+  variants: {
+    borderWidth: ['responsive', 'hover', 'focus'],
+    borderStyle: ['responsive', 'hover', 'focus'],
+  }
+}
